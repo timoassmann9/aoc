92fb4f90197e13dfbea8e434706724802d5e71a8
timoassmann9 <timo.assmann@web.de>
2025-12-03 15:46:16 +0100

    progress 2015/day4

--- 2015/day4/part1.py
+++ 2015/day4/part1.py
@@ -1,5 +1,13 @@
 import hashlib
 
-res = hashlib.md5(b"abc")
-hex = res.hexdigest()
-print(hex.startswith("9000"))
+key_start = "iwrupvqb"
+
+for i in range(1000000):
+    key = key_start + str(i)
+    res = hashlib.md5(key.encode())
+    hex = res.hexdigest()
+
+    leading_zeros = 0
+    for idx, char in enumerate(hex):
+        if idx == 0 and char == "0":
+            leading_zeros += 1

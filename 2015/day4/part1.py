f18ddabcd7e6f45847cf17392e0fc2679d93dd50
timoassmann9 <timo.assmann@web.de>
2025-12-03 16:06:41 +0100

    complete 2015/day4

--- 2015/day4/part1.py
+++ 2015/day4/part1.py
@@ -8,6 +8,16 @@ for i in range(1000000):
     hex = res.hexdigest()
 
     leading_zeros = 0
-    for idx, char in enumerate(hex):
-        if idx == 0 and char == "0":
+    if hex[0] == "0":
+        leading_zeros += 1
+
+    for idx in range(1, len(hex)):
+        if hex[idx] == "0" and hex[idx-1] == "0":
             leading_zeros += 1
+        else:
+            break
+
+    if leading_zeros > 4:
+        print(key)
+        print(hex)
+        break
---
+++ 2015/day4/part2.py
@@ -0,0 +1,23 @@
+import hashlib
+
+key_start = "iwrupvqb"
+
+for i in range(10000000):
+    key = key_start + str(i)
+    res = hashlib.md5(key.encode())
+    hex = res.hexdigest()
+
+    leading_zeros = 0
+    if hex[0] == "0":
+        leading_zeros += 1
+
+    for idx in range(1, len(hex)):
+        if hex[idx] == "0" and hex[idx-1] == "0":
+            leading_zeros += 1
+        else:
+            break
+
+    if leading_zeros > 5:
+        print(key)
+        print(hex)
+        break

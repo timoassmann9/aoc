28f89a8bb4d1d0170957252f9cd78e6f1f61f908
timoassmann9 <timo.assmann@web.de>
2025-12-04 15:02:43 +0100

    complete 2025/day3

--- 2025/day3/part2.py
+++ 2025/day3/part2.py
@@ -1,15 +1,25 @@
+def get_max_joltage(bank: str, digits):
+    result = ""
+    start = 0
+
+    for pos in range(digits):
+        remaining = digits - pos
+        end = len(bank) - remaining
+        best_char = max(bank[start:end+1])
+        best_index = bank.index(best_char, start, end+1)
+
+        result += best_char
+        start = best_index + 1
+
+    return result
+
 with open("input.txt", "r") as f:
     data = f.read()
 
 banks = data.split()
 max_joltages = list()
 for bank in banks:
-    possible_joltages = list()
-    for i in range(len(bank)-1):
-        for j in range(i+1, len(bank)):
-            joltage = int(bank[i] + bank[j])
-            possible_joltages.append(joltage)
-    max_joltage = max(possible_joltages)
-    max_joltages.append(max_joltage)
+    max_joltage = get_max_joltage(bank, 12)
+    max_joltages.append(int(max_joltage))
 
 print(sum(max_joltages))

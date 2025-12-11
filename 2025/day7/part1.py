a13c23457eeccc34f3162ae970e28cb94189be3d
timoassmann9 <timo.assmann@web.de>
2025-12-11 16:30:02 +0100

    update 2025/day7

--- 2025/day7/part1.py
+++ 2025/day7/part1.py
@@ -1,5 +1,22 @@
 with open("input.txt") as f:
     data = f.read()
+    
+total_splits = 0
 
-# S->Beam wird darunter erstellt
-# 
+data = data.split()
+data = [list(x) for x in data]
+
+for i, line in enumerate(data):
+    for j, char in enumerate(line):
+        if char == "S":
+            data[i+1][j] = "|"
+            break
+        if data[i-1][j] == "|":
+            if char == "^":
+                total_splits += 1
+                data[i][j-1] = "|"
+                data[i][j+1] = "|"
+            if char == ".":
+                data[i][j] = "|"
+
+print(total_splits)
